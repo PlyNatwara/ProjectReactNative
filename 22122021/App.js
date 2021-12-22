9fabde8cb027731c6e29bb3146160051d7fb3613
PlyNatwara <ar.natwara_st@tni.ac.th>
2021-12-22 11:50:13 +0700

    WebView & Stack Navigation

---
+++ 22122021/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import AboutScreen from './screens/AboutScreen';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#0096DA',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'HomeScreen'}}
+        />
+
+        <Stack.Screen
+          name="About"
+          component={AboutScreen}
+          options={{title: 'AboutUs'}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
